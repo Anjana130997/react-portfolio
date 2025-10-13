8bdd67549a874044ec250041da5b284601d3f6c0
Anjana E S <anjana@example.com>
2025-10-13 20:47:49 +0530

    made gitstat changes

--- react_portfolio/src/components/GitHubStats.jsx
+++ react_portfolio/src/components/GitHubStats.jsx
@@ -5,11 +5,12 @@ import VanillaTilt from "vanilla-tilt";
 export default function GitHubStats() {
   const tiltRef = useRef([]);
 
-  const githubImages = [
-    "https://github-readme-stats.vercel.app/api/top-langs/?username=Anjana130997&theme=vue-dark&bg_color=1c1c1c&text_color=f0f0f0&icon_color=73cae7&card_width=300px&title_color=73cae7&show_icons=true&hide_border=true&layout=compact",
-    "https://github-readme-streak-stats.herokuapp.com/?user=Anjana130997&theme=shadow-brown&card_width=420&card_height=100&background=1c1c1c&stroke=f3a296&ring=f0f0f0&currStreakNum=73cae7&sideLabels=f0f0f0&fire=EB4506&sideNums=f0f0f0&border=1c1c1c",
-    "https://github-readme-stats.vercel.app/api?username=Anjana130997&bg_color=1c1c1c&text_color=f0f0f0&icon_color=73cae7&title_color=73cae7&card_width=300px&show_icons=true&hide_border=true&count_private=true"
-  ];
+const githubImages = [
+  `https://github-readme-stats.vercel.app/api/top-langs/?username=Anjana130997&theme=vue-dark&bg_color=1c1c1c&text_color=f0f0f0&icon_color=73cae7&card_width=300px&title_color=73cae7&show_icons=true&hide_border=true&layout=compact&cache_seconds=3600`,
+  `https://github-readme-streak-stats.herokuapp.com/?user=Anjana130997&theme=shadow-brown&card_width=420&card_height=100&background=1c1c1c&stroke=f3a296&ring=f0f0f0&currStreakNum=73cae7&sideLabels=f0f0f0&fire=EB4506&sideNums=f0f0f0&border=1c1c1c`,  
+  `https://github-readme-stats.vercel.app/api?username=Anjana130997&bg_color=1c1c1c&text_color=f0f0f0&icon_color=73cae7&title_color=73cae7&card_width=300px&show_icons=true&hide_border=true&count_private=true&cache_seconds=3600`
+];
+
 
   useEffect(() => {
     tiltRef.current.forEach((el) => {
